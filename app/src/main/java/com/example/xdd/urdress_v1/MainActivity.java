a77e6c4a147fd29f828d59ea9de2da3d0758553e
marcoabonce <43801901+marcoabonce@users.noreply.github.com>
2018-10-05 19:14:05 -0500

    EL RETOÑO ES PVTO

--- app/src/main/java/com/example/xdd/urdress_v1/MainActivity.java
+++ app/src/main/java/com/example/xdd/urdress_v1/MainActivity.java
@@ -1,30 +1,24 @@
 package com.example.xdd.urdress_v1;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-/*
 import org.ksoap2.SoapEnvelope;
 import org.ksoap2.serialization.SoapObject;
 import org.ksoap2.serialization.SoapPrimitive;
 import org.ksoap2.serialization.SoapSerializationEnvelope;
-import org.ksoap2.transport.HttpTransportSE;*/
-import org.xmlpull.v1.XmlPullParserException;
-
-import java.io.IOException;
-import java.net.Proxy;
-
+import org.ksoap2.transport.HttpTransportSE;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView crearCuenta;
-    private static final String NAMESPACE = "http://drees.siidmex.com.mx/wsAppDress.asmx/";
-    private static final String MAIN_REQUEST_URL = "http://drees.siidmex.com.mx/wsAppDress.asmx";
-    private static final String SOAP_ACTION = "http://drees.siidmex.com.mx/wsAppDress.asmx/loguin";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,37 +32,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-/*
-        String methodname = "logui";
-        SoapObject request = new SoapObject(NAMESPACE, methodname);
-        request.addProperty("contrasenia", "20barney");
-        request.addProperty("dato", "marcoabonce@hotmail.com");
-        request.addProperty("esFacebook", "false");
-        request.addProperty("idTipoDispositivo", "0");
-        request.addProperty("idTipoLoguin", "0");
-        request.addProperty("idTipoUsuario", "0");
-        request.addProperty("tokenDispositivo", "");
 
-        SoapSerializationEnvelope envelope = getSoapSerializationEnvelope(request) ;
+        SegundoPlano tarea = new SegundoPlano();
+        tarea.execute();
 
 
-
-        HttpTransportSE ht = getHttpTransportSE();
-        String data =null;
-        try {
-            ht.call(SOAP_ACTION, envelope);
-            SoapPrimitive resultsString = (SoapPrimitive)envelope.getResponse();
-            data = resultsString.toString();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (XmlPullParserException e) {
-            e.printStackTrace();
-        }
-
-        Toast.makeText(getApplicationContext(), data, Toast.LENGTH_LONG).show();
-
-        */
+    }
         /*AltaUsuario alta = retrofit.create(AltaUsuario.class);
         Call<Resp> usuarioCall = alta.getUsu(1,"maeeaahh@gmail.com", "marhjaaakhcgaaa",
                 "xd","xd","xd","xd", "cf",
@@ -95,10 +64,59 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-        */
+    }*/
+
+
+    private class SegundoPlano extends AsyncTask<Void,Void,Void> {
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            loguinUsuario();
+            return null;
+        }
     }
 
-/*
+    private void loguinUsuario (){
+         String METHODNAME="loguin";
+         String NAMESPACE = "http://drees.siidmex.com.mx/";
+         String MAIN_REQUEST_URL = "http://drees.siidmex.com.mx/wsAppDress.asmx";
+         String SOAP_ACTION = "http://tempuri.org/loguin";
+         SoapPrimitive resultsString=null;
+
+        try{
+            SoapObject request = new SoapObject(NAMESPACE, METHODNAME);
+            request.addProperty("contrasenia", "20barney");
+            request.addProperty("dato", "marcoabonce@hotmail.com");
+            request.addProperty("esFacebook", "false");
+            request.addProperty("idTipoDispositivo", "0");
+            request.addProperty("idTipoLoguin", "0");
+            request.addProperty("idTipoUsuario", "0");
+            request.addProperty("tokenDispositivo", "xdd");
+
+            SoapSerializationEnvelope soapEnvelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+            soapEnvelope.dotNet = true;
+            soapEnvelope.implicitTypes = true;
+            soapEnvelope.setAddAdornments(false);
+            soapEnvelope.setOutputSoapObject(request);
+
+            HttpTransportSE transport = new HttpTransportSE(MAIN_REQUEST_URL);
+            transport.call(SOAP_ACTION,soapEnvelope);
+            resultsString=(SoapPrimitive) soapEnvelope.getResponse();
+
+            Toast.makeText(this,"SUPER CHIDO :V "+resultsString.toString(), Toast.LENGTH_LONG).show();
+        }catch(Exception e){
+            Log.e("Error ","Problema"+e.getMessage());
+        }
+    }
+
     private final SoapSerializationEnvelope getSoapSerializationEnvelope(SoapObject request) {
         SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
         envelope.dotNet = true;
@@ -109,20 +127,18 @@ public class MainActivity extends AppCompatActivity {
         return envelope;
     }
 
+    /*
     private final HttpTransportSE getHttpTransportSE() {
         HttpTransportSE ht = new HttpTransportSE(Proxy.NO_PROXY,MAIN_REQUEST_URL,60000);
         ht.debug = true;
         ht.setXmlVersionTag("<!--?xml version=\"1.0\" encoding= \"UTF-8\" ?-->");
         return ht;
-    }
-*/
+    }*/
+
+
 
 }
 
-/*
 
 
-keytool -exportcert -alias androiddebugkey -keystore "C:\Users\Dell\.android\debug.keystore" | "C:\Users\Dell\Downloads\openssl-0.9.8k_X64\bin\openssl" sha1 -binary | "C:\Users\Dell\Downloads\openssl-0.9.8k_X64\bin\openssl" base64
-
-
- */
+//keytool -exportcert -alias androiddebugkey -keystore "C:\Users\Dell\.android\debug.keystore" | "C:\Users\Dell\Downloads\openssl-0.9.8k_X64\bin\openssl" sha1 -binary | "C:\Users\Dell\Downloads\openssl-0.9.8k_X64\bin\openssl" base64
